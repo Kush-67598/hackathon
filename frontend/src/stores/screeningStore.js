090873f61566c82a0bf2f2187268086a1dd2527a
Kush <kushsingh2666@gmail.com>
2026-04-19 11:58:34 +0530

    fixed some

--- frontend/src/stores/screeningStore.js
+++ frontend/src/stores/screeningStore.js
@@ -3,7 +3,8 @@ import { create } from "zustand";
 export const useScreeningStore = create((set) => ({
   latestSessionId: "",
   latestOutput: null,
+  conditionDetails: null,
   history: [],
-  setLatestResult: (sessionId, output) => set({ latestSessionId: sessionId, latestOutput: output }),
+  setLatestResult: (sessionId, output, conditionDetails = null) => set({ latestSessionId: sessionId, latestOutput: output, conditionDetails }),
   setHistory: (history) => set({ history }),
 }));
